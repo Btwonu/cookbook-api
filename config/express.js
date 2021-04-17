2205ac3f730b12a09ccde0fc741c26b239008273
Val Delchev <btwonu@gmail.com>
2021-04-17 11:59:33 +0300

    Add config files

--- config/express.js
+++ config/express.js
@@ -1,5 +1,23 @@
 const express = require('express');
+const handlebars = require('express-handlebars');
 
 module.exports = (app) => {
+  const hbs = handlebars.create({
+    extname: 'hbs',
+    helpers: {
+      whatis: function (param) {
+        console.log('THIS IS:', param);
+      },
+    },
+  });
+
+  app.engine('hbs', hbs.engine);
+
+  app.set('view engine', 'hbs');
+
+  app.use(express.static('public'));
+
   app.use(express.urlencoded({ extended: false }));
+
+  // app.use(express.json());
 };
