790f56fcbd2959d86347402988cd8a2eff64ba5a
Val Delchev <btwonu@gmail.com>
2021-05-19 15:34:52 +0300

    change port

--- config/index.js
+++ config/index.js
@@ -1,5 +1,5 @@
 module.exports = {
-  port: process.env.PORT || 3000,
+  port: process.env.PORT || 5000,
   DB: {
     uri: 'mongodb://localhost:27017/cookbook-test',
     options: { useNewUrlParser: true, useUnifiedTopology: true },
