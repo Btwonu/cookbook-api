c44fa83d7b4c08149ce430b5107816951e98172a
Val Delchev <btwonu@gmail.com>
2021-07-08 21:22:18 +0300

    explicitly set localhost domain on cookie

--- config/express.js
+++ config/express.js
@@ -25,6 +25,7 @@ module.exports = (app) => {
       resave: false,
       saveUninitialized: true,
       cookie: {
+        domain: 'http://localhost:3000',
         httpOnly: true,
         maxAge: 1000 * 60 * 60 * 24 * 7, // 1 week
         sameSite: 'none',
